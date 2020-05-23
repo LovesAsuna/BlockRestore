a0bd27dbb2cef3a4e69320b6a83c409f47232662
梦曦·花已落 <qq625924077@gmail.com>
2020-05-23 18:25:44 +0800

    [O] 优化以前写的一坨屎

--- src/main/java/org/sct/blockrestore/util/Timer.kt
+++ src/main/java/org/sct/blockrestore/util/Timer.kt
@@ -3,6 +3,7 @@ package org.sct.blockrestore.util
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.sct.blockrestore.BlockRestore
+import org.sct.blockrestore.BlockRestore.Companion.instance
 import org.sct.blockrestore.data.BlockRestoreData.blockList
 import org.sct.blockrestore.data.BlockRestoreData.locationList
 import org.sct.blockrestore.data.BlockRestoreData.locationMaterial
@@ -10,40 +11,46 @@ import org.sct.blockrestore.data.BlockRestoreData.locationTime
 
 class Timer {
     fun run() {
-        Bukkit.getScheduler().runTaskTimer(BlockRestore.instance, Runnable {
+        Bukkit.getScheduler().runTaskTimer(instance, Runnable {
             val nowTime = System.currentTimeMillis() / 1000
             for (lt in locationList) {
-                var skip = true //是否跳过坐标
-                var material: Material? = null //此刻坐标的材质
-                if (locationMaterial[lt] != null) {
-                    material = locationMaterial[lt] //获取此刻坐标的材质
+                /*是否跳过坐标*/
+                var skip: Boolean
+                /*此刻坐标的材质*/
+                val material = locationMaterial[lt]
+
+                /*判断此时位置的材质是否在列表里*/
+                skip = blockList.parallelStream().anyMatch {
+                    material == Material.getMaterial(it)
                 }
-                for (block in blockList) { //判断此时位置的材质是否在列表里
-                    if (material == Material.getMaterial(block)) {
-                        skip = false
-                    }
-                }
+
+                /*如果不在列表内,跳过此处坐标*/
                 if (skip) {
-                    continue  //如果不在列表内,跳过此处坐标
+                    continue
                 }
-                val time = BlockRestore.instance.config.getInt("blocks.$material.restoretime")
-                val replace = Material.getMaterial(BlockRestore.instance.config.getString("blocks.$material.replaceblock")!!)
+                val restoreTime = instance.config.getInt("Blocks.$material.RestoreTime")
+                val replaceBlock = Material.getMaterial(instance.config.getString("Blocks.$material.ReplaceBlock")!!)
                 if (locationTime[lt] != null) {
-                    if (nowTime - locationTime[lt]!! + 1 >= time) { //时间大于预设时间
-                        //System.out.println("时间大于delay");
-                        if (BlockRestore.instance.config.getBoolean("blocks.$material.replace")) { //如果replacerestore开启
-                            if (lt.block.type == Material.AIR) { //如果等于空气,恢复成替换方块
-                                //System.out.println("如果等于空气,恢复成替换方块");
-                                lt.block.type = replace!!
-                                locationTime[lt] = System.currentTimeMillis() / 1000
-                            } else if (lt.block.type == replace) { //如果等于替换方块,恢复成原始方块
-                                //System.out.println("如果等于替换方块,恢复成原始方块");
-                                lt.block.type = locationMaterial[lt]!!
-                                locationTime.remove(lt)
-                                locationMaterial.remove(lt)
-                                locationList.remove(lt)
+                    /*时间大于预设时间*/
+                    if (nowTime - locationTime[lt]!! + 1 >= restoreTime) {
+                        /*如果ReplaceRestore开启*/
+                        if (instance.config.getBoolean("Blocks.$material.Replace")) {
+                            when (lt.block.type) {
+                                /*如果等于空气,恢复成替换方块*/
+                                Material.AIR -> {
+                                    lt.block.type = replaceBlock!!
+                                    locationTime[lt] = System.currentTimeMillis() / 1000
+                                }
+                                /*如果等于替换方块,恢复成原始方块*/
+                                replaceBlock -> {
+                                    lt.block.type = locationMaterial[lt]!!
+                                    locationTime.remove(lt)
+                                    locationMaterial.remove(lt)
+                                    locationList.remove(lt)
+                                }
                             }
-                        } else { //如果replacerestore关闭
+                        } else {
+                            /*ReplaceRestore关闭*/
                             if (lt.block.type == Material.AIR) { //如果等于空气,恢复成原始方块
                                 lt.block.type = locationMaterial[lt]!!
                                 locationTime.remove(lt)
